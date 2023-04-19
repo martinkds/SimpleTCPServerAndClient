ca55e049a9d7356fb47bba9aba82e737b8363fcc
Martin Ortega-M <martin.ortega@kairosds.com>
2023-04-19 00:16:52 -0600

    Initial commit

---
+++ SimpleTCPClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Samples.Networking
+{
+    public class SimpleTCPClient
+    {
+        public static void Main(string[]args)
+        {
+			Console.WriteLine ("Press [Enter] to read the message from the server");
+			Console.ReadLine ();
+            try
+            {
+                TcpClient socket = new TcpClient("127.0.0.1",5432);
+                NetworkStream inputStream = socket.GetStream();
+                using(StreamReader reader = new StreamReader(inputStream)){
+					string message = reader.ReadLine();
+                    Console.WriteLine("Message from server:" + message);
+                }
+		Console.WriteLine();
+                inputStream.Close();
+				socket.Close();
+            }
+            catch(SocketException socketException){
+                Console.WriteLine("SocketException: " + socketException.Message);
+            }
+            catch (System.Exception exception){
+                Console.WriteLine("Exception: " + exception.Message);
+            }
+        }
+    }
+} 
---
+++ SimpleTCPServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace SimpleTCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+			{
+				TcpListener listener = new TcpListener(IPAddress.Loopback,5432);
+				listener.Start();
+				Console.WriteLine("Waiting for a connection...");
+				TcpClient socket = listener.AcceptTcpClient();
+				NetworkStream outputStream = socket.GetStream();
+				using(StreamWriter writer = new StreamWriter(outputStream))
+				{
+					writer.WriteLine("Hello World to the client!");
+				}
+				outputStream.Close();
+				socket.Close();
+			}
+			catch (System.Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+        }
+    }
+}
